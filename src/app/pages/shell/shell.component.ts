eb56afefa291c228f7683831e5813595916c9cca
JoeyWNK <JoeyWNK@users.noreply.github.com>
2020-03-04 19:53:15 -0800

    Xterm Fix attemps

--- src/app/pages/shell/shell.component.ts
+++ src/app/pages/shell/shell.component.ts
@@ -1,10 +1,11 @@
 import { Component, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChange, ViewChild } from "@angular/core";
 import { TranslateService } from "@ngx-translate/core";
 import { MatDialog } from '@angular/material';
-import { ShellService, WebSocketService } from "../../services/";
+import { ShellService, WebSocketService, AttachAddon } from "../../services/";
 import helptext from "./../../helptext/shell/shell";
 import { CopyPasteMessageComponent } from "./copy-paste-message.component";
-
+import { Terminal } from 'xterm';
+import { FitAddon } from 'xterm-addon-fit';
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html',
@@ -21,9 +22,9 @@ export class ShellComponent implements OnInit, OnChanges, OnDestroy {
   rows: string;
   font_size = 14;
   public token: any;
-  public xterm: any;
+  public xterm: Terminal;
+  private fitAddon: FitAddon = new FitAddon();
   public resize_terminal = true;
-  private shellSubscription: any;
 
   public usage_tooltip = helptext.usage_tooltip;
 
@@ -31,13 +32,12 @@ export class ShellComponent implements OnInit, OnChanges, OnDestroy {
   public shellConnected: boolean = false;
 
   ngOnInit() {
+    this.xterm = new Terminal();
+    this.xterm.open(this.container.nativeElement);
+    this.xterm.loadAddon(this.fitAddon);
+    this.onResize(null);
     this.getAuthToken().subscribe((res) => {
       this.initializeWebShell(res);
-      this.shellSubscription = this.ss.shellOutput.subscribe((value) => {
-        if (value !== undefined) {
-          this.xterm.write(value);
-        }
-      });
       this.initializeTerminal();
     });
   }
@@ -46,13 +46,17 @@ export class ShellComponent implements OnInit, OnChanges, OnDestroy {
     if (this.ss.connected){
       this.ss.socket.close();
     }
-    if(this.shellSubscription){
-      this.shellSubscription.unsubscribe();
-    }
   }
   
-  onResize(event) {
-    this.resizeTerm();
+  onResize(event){
+    let dims = this.fitAddon.proposeDimensions();
+    if (isNaN(dims.rows) || dims.rows == Infinity || isNaN(dims.cols) || dims.cols == Infinity) {
+      console.debug(`Remove an bug where dimensions of the detached terminal element aren't set`)
+      this.xterm.resize(10, 10);
+    } else {
+      this.fitAddon.fit();
+    }
+    this.informTerminalChange();
   }
 
   resetDefault() {
@@ -78,42 +82,10 @@ export class ShellComponent implements OnInit, OnChanges, OnDestroy {
   }
 
   initializeTerminal() {
-    const domHeight = document.body.offsetHeight;
-    const domWidth = document.body.offsetWidth;
-    let colNum = (domWidth * 0.75 - 104) / 10;
-    if (colNum < 80) {
-      colNum = 80;
-    }
-    let rowNum = (domHeight * 0.75 - 104) / 21;
-    if (rowNum < 10) {
-      rowNum = 25;
-    }
-
-    this.xterm = new (<any>window).Terminal({
-      'cursorBlink': false,
-      'tabStopWidth': 8,
-      'cols': parseInt(colNum.toFixed(),10),
-      'rows': parseInt(rowNum.toFixed(),10),
-      'focus': true
-    });
-    this.xterm.open(this.container.nativeElement, true);
-    this.xterm.attach(this.ss);
-    this.xterm._initialized = true;
-  }
-
-  resizeTerm(){
-    const domHeight = document.body.offsetHeight;
-    const domWidth = document.body.offsetWidth;
-    let colNum = (domWidth * 0.75 - 104) / 10;
-    if (colNum < 80) {
-      colNum = 80;
-    }
-    let rowNum = (domHeight * 0.75 - 104) / 21;
-    if (rowNum < 10) {
-      rowNum = 10;
-    }
-    this.xterm.resize(parseInt(colNum.toFixed(),10),parseInt(rowNum.toFixed(),10));
-    return true;
+    const attachAddon = new AttachAddon(this.ss.socket);
+    this.xterm.loadAddon(attachAddon);
+    this.fitAddon.fit();
+    //this.informTerminalChange();
   }
 
   initializeWebShell(res: string) {
@@ -127,6 +99,16 @@ export class ShellComponent implements OnInit, OnChanges, OnDestroy {
 
   getAuthToken() {
     return this.ws.call('auth.generate_token');
+  }
+
+  informTerminalChange(){
+    let dims = this.fitAddon.proposeDimensions();
+    if (isNaN(dims.rows) || dims.rows == Infinity || isNaN(dims.cols) || dims.cols == Infinity) {
+
+    } else if (this.ss.connected){
+       this.ss.send(`\u001b[2J`);
+    }
+   
   }
 
   reconnect() {
